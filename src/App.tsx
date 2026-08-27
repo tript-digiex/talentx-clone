d741f1f51f88d5c5857e59e6e25fb50d4c710388
Tri Pham <tri.pt@digiex.group>
2026-08-27 09:44:54 +0700

    chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+function App() {
+  return <div>
+    <div>Hello, Vite + React!</div>
+    {/* <div><Button variant="default" >Click me</Button></div> */}
+  </div>;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
